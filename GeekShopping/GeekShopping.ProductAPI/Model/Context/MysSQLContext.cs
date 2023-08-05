7ad0802257bd97ff41dbdc29923cf1bfbb3e66c0
freaklucas <lucas.oliveira.freak@gmail.com>
2023-08-05 15:48:02 -0300

    init

---
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MysSQLContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Model.Context
+{
+    public class MysSQLContext : DbContext
+    {
+        public MysSQLContext() {}
+        public MysSQLContext(DbContextOptions <MysSQLContext> options) : base(options) {}
+        public DbSet<Product> Products { get; set; }
+    }
+}
